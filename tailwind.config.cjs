f9347d1e115f9ea09f25625637e668d12bfb2d03
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-23 17:47:39 +0300

    Add background images for homepage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "home-page-desktop":
+          "url('./public/assets/home/background-home-desktop.jpg')",
+        "home-page-tablet":
+          "url('./public/assets/home/background-home-tablet.jpg')",
+        "home-page-mobile":
+          "url('./public/assets/home/background-home-mobile.jpg')",
+      },
+    },
   },
   plugins: [],
 };
