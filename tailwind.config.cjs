36d2bd9119591ae46b3863418bb3289bdb7adfb2
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-25 15:13:06 +0300

    Add custom colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        dark: "hsl(230, 35%, 7%)",
+        navBg: "#151923",
+        lightBlue: "rgb(208, 214, 249)",
+        borderColor: "rgb(255, 255, 255, 0.6)",
+      },
       backgroundImage: {
         "home-page-desktop": "url('/assets/home/background-home-desktop.jpg')",
         "home-page-tablet": "url('/assets/home/background-home-tablet.jpg')",
