c1202c017d133e893a4142125b047085f013b0bb
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-25 10:12:00 +0300

    rename pahes to container

---
+++ src/containers/Landing.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const Landing = () => {
+  return (
+    <div>
+      <nav className="bg-black w-full h-16"></nav>
+      <h1>landing</h1>
+    </div>
+  );
+};
+
+export default Landing;
