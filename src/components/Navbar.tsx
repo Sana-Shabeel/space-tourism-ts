9f0137713b0fb489f4fb9f7fe495bf1fc1fa67b5
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-30 14:41:55 +0300

    finish setting up navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,17 +10,33 @@ const Navbar = () => {
     { num: "03", title: "TECHNOLOGY" },
   ];
   return (
-    <nav className="py-6 px-4 flex justify-between items-center font-barlowCond md:h-24">
+    <nav className="text-white flex justify-between items-center font-barlowCond md:h-24 mx-8 py-6 md:pl-4 md:mx-0 md:py-0">
       <div className="w-10 md:w-12">
         <img src="/assets/shared/logo.svg" alt="logo" />
       </div>
 
+      <ul className="hidden md:flex w-3/5 gap-6 h-full items-center justify-around bg-alphaWhite backdrop-blur-lg lg:mt-12 px-12 lg:w-3/4	">
+        {navbar.map(({ num, title }) => (
+          <li className="text-lg tracking-widest font-extralight h-full flex justify-center items-center hover:border-b-2">
+            <a href="">
+              <span className="md:hidden lg:inline-block font-bold mr-3">
+                {num}
+              </span>
+              {title}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      {/* mobile navbar */}
       <div className="md:hidden">
         <div className="cursor-pointer" onClick={() => setToggleNavbar(true)}>
           <img src="/assets/shared/icon-hamburger.svg" alt="burger menu" />
         </div>
         {toggleNavbar && (
-          <div className="fixed inset-y-0	right-0 left-1/3 bg-alphaWhite backdrop-blur-lg px-8">
+          <div
+            className={`fixed inset-y-0 right-0 left-1/3 bg-alphaWhite backdrop-blur-lg px-4 transition-all	 duration-500 ease-in-out`}
+          >
             <div
               className="my-8 flex justify-end"
               onClick={() => setToggleNavbar(false)}
@@ -30,10 +46,7 @@ const Navbar = () => {
 
             <ul className="mt-12 sm:ml-8 sm:mt-16">
               {navbar.map(({ num, title }, idx) => (
-                <li
-                  key={idx}
-                  className="text-white ml-4 my-10 text-xl tracking-linkSpace"
-                >
+                <li key={idx} className="ml-4 my-10 text-xl tracking-linkSpace">
                   <a href={`#${title}`} className="font-extralight">
                     <span className="mr-4 font-semibold">{num}</span>
                     {title}
