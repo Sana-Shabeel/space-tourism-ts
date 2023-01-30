4262fadc1c85433dbe4f8001c0c4aa83c46a38fc
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-30 16:41:32 +0300

    Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
-
-import Landing from "./pages/Landing";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <main className="min-h-screen lg:pt-6 bg-home-page-mobile sm:bg-home-page-tablet lg:bg-home-page-desktop bg-cover bg-center">
       <Navbar />
+
+      <Homepage />
     </main>
   );
 }
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,10 +2,22 @@ import React, { useState } from "react";
 
 const Landing = () => {
   return (
-    <div>
-      <nav className="bg-black w-full h-16"></nav>
-      <h1>landing</h1>
-    </div>
+    <section>
+      <div>
+        <h1>
+          SO, YOU WANT TO TRAVEL TO <span>SPACE</span>
+        </h1>
+        <div>
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </div>
+      </div>
+      <div>
+        <span>EXPLORE</span>
+      </div>
+    </section>
   );
 };
 
