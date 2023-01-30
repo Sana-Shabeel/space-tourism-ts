a22af7aa5b12d748eed1a1f56a517140ec42f4ef
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-30 20:47:44 +0300

    home page header

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,14 +2,16 @@ import React, { useState } from "react";
 
 const Landing = () => {
   return (
-    <section className="h-screen flex flex-col text-lightBlue">
+    <section className="h-screen flex flex-col text-lightBlue mt-6">
       <div>
-        <h1 className="font-barlowCond text-fs300 text-center tracking-widest">
+        <h1 className="font-barlowCond text-fs300 text-center tracking-h1space">
           SO, YOU WANT TO TRAVEL TO{" "}
-          <span className="text-fs800 block">SPACE</span>
+          <span className="text-fs800 block text-white font-bellefair">
+            SPACE
+          </span>
         </h1>
-        <div>
-          <p className="text-fs200">
+        <div className="mx-6">
+          <p className="text-fs200 text-center">
             Let’s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we’ll give you a truly out of this
