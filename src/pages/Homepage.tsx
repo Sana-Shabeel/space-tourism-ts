bfb5f2dbb529810c5321e2845429cedf57f2cad3
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-30 21:34:16 +0300

    hompe page mobile

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -11,7 +11,7 @@ const Landing = () => {
           </span>
         </h1>
         <div className="mx-6">
-          <p className="text-fs200 text-center">
+          <p className="text-fs200 text-center leading-7	">
             Let’s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we’ll give you a truly out of this
@@ -19,8 +19,10 @@ const Landing = () => {
           </p>
         </div>
       </div>
-      <div>
-        <span>EXPLORE</span>
+      <div className="w-40 h-40 rounded-full bg-white grid place-items-center mx-auto my-14">
+        <span className="text-dark font-bellefair text-fs400 tracking-widest">
+          EXPLORE
+        </span>
       </div>
     </section>
   );
