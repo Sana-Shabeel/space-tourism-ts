698a0e0ed116014c028a1cf5a9395472a7e29818
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-30 17:39:26 +0300

    Home page skeleton

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,16 +2,19 @@ import React, { useState } from "react";
 
 const Landing = () => {
   return (
-    <section>
+    <section className="h-screen flex flex-col text-lightBlue">
       <div>
-        <h1>
-          SO, YOU WANT TO TRAVEL TO <span>SPACE</span>
+        <h1 className="font-barlowCond text-fs300 text-center tracking-widest">
+          SO, YOU WANT TO TRAVEL TO{" "}
+          <span className="text-fs800 block">SPACE</span>
         </h1>
         <div>
-          Let’s face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
-          experience!
+          <p className="text-fs200">
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
         </div>
       </div>
       <div>
