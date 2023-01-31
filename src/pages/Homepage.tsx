f5ab4cb611b03ce0204fb023f516dd7eb40e4121
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-31 19:17:35 +0300

    finished large screen layout

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,16 +5,16 @@ const Landing = () => {
   return (
     <section className="grid min-h-screen grid-rows-home  bg-home-page-mobile bg-cover bg-center text-lightBlue sm:bg-home-page-tablet lg:bg-home-page-desktop ">
       <Navbar />
-      <div className="flex h-full flex-col pt-6 ">
-        <div className="m-auto w-80 md:w-3/5">
-          <h1 className="text-center font-barlowCond text-fs400 tracking-h1space">
+      <div className="flex h-full flex-col pt-6 lg:flex-row ">
+        <div className="m-auto w-80 md:w-3/5 lg:w-450px">
+          <h1 className="text-center font-barlowCond text-fs400 tracking-h1space lg:text-left">
             SO, YOU WANT TO TRAVEL TO{" "}
             <span className="block font-bellefair text-fs750 text-white md:text-fs900">
               SPACE
             </span>
           </h1>
           <div>
-            <p className="text-center text-fs200 font-extralight	leading-7 tracking-wide">
+            <p className="text-center text-fs200 font-extralight	leading-7 tracking-wide lg:text-left lg:text-fs400">
               Let’s face it; if you want to go to space, you might as well
               genuinely go to outer space and not hover kind of on the edge of
               it. Well sit back, and relax because we’ll give you a truly out of
@@ -22,7 +22,7 @@ const Landing = () => {
             </p>
           </div>
         </div>
-        <div className="mx-auto my-14 grid h-40 w-40 place-items-center rounded-full bg-white md:h-60 md:w-60 ">
+        <div className="mx-auto my-14 grid h-40 w-40 place-items-center rounded-full bg-white md:h-60 md:w-60 lg:h-72	lg:w-72	lg:self-end">
           <span className="font-bellefair text-fs400 tracking-widest text-dark md:text-fs600">
             EXPLORE
           </span>
