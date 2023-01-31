4e05190ed1aa8b8aebd2e1bc8c85c51eaee9b521
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-01-31 17:28:10 +0300

    remove unused class name

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <main className="min-h-full lg:pt-6 bg-home-page-mobile sm:bg-home-page-tablet lg:bg-home-page-desktop bg-cover bg-center">
-      <Navbar />
-
+    <main>
       <Homepage />
     </main>
   );
