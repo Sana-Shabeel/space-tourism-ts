9eccacbcb6e488d8d78ca264587b009492d4d19e
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-01 16:55:38 +0300

    Destination page

---
+++ src/pages/Destination.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Destination = () => {
+  return (
+    <section className="min-h-screen bg-destination-mobile bg-cover bg-center sm:bg-destination-tablet">
+      <Navbar />
+    </section>
+  );
+};
+
+export default Destination;
