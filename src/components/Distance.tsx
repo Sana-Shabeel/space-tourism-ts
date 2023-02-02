8f25e875dd044a0b9c0820c0b809041102e36836
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-02 20:02:39 +0300

    Add styles for large screen

--- src/components/Distance.tsx
+++ src/components/Distance.tsx
@@ -8,10 +8,10 @@ interface Props {
 export const Distance = ({ title, description }: Props) => {
   return (
     <div className="py-3">
-      <p className="mb-3 text-center font-barlowCond text-fs300 font-light uppercase tracking-2xl text-lightBlue">
+      <p className="mb-3 text-center font-barlowCond text-fs300 font-light uppercase tracking-2xl text-lightBlue lg:text-left lg:text-fs200">
         {title}
       </p>
-      <h3 className="text-center font-bellefair text-fs500 font-light uppercase text-white">
+      <h3 className="text-center font-bellefair text-fs500 font-light uppercase text-white lg:text-left">
         {description}
       </h3>
     </div>
