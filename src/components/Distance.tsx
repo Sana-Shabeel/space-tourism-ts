1272cbe69c68c6755dca5358498504ebfa49674d
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-02 16:49:49 +0300

    Add destination page to App

---
+++ src/components/Distance.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface Props {
+  title: string;
+  description: string;
+}
+
+export const Distance = ({ title, description }: Props) => {
+  return (
+    <div className="py-3">
+      <p className="mb-3 text-center font-barlowCond text-fs300 font-light uppercase tracking-2xl text-lightBlue">
+        {title}
+      </p>
+      <h3 className="text-center font-bellefair text-fs500 font-light uppercase text-white">
+        {description}
+      </h3>
+    </div>
+  );
+};
