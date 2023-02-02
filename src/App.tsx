1c94a6b927dc37354fc2b7b97584662301728ae5
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-02 16:50:17 +0300

    Add destination page to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import Destination from "./pages/Destination";
 import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <main>
-      <Homepage />
+      <Destination />
     </main>
   );
 }
