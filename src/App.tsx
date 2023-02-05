88905930a29c1abb7c6c0d3f3b95c1d733cdfc5f
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-05 14:38:42 +0300

    Crew page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import Crew from "./pages/Crew";
 import Destination from "./pages/Destination";
 import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <main>
-      <Destination />
+      <Crew />
     </main>
   );
 }
