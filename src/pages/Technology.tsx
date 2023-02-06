9553ad89aabbc9074fbdd72806563cbb1be544c8
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-06 13:19:19 +0300

    Technology page

---
+++ src/pages/Technology.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+export const Technology = () => {
+  return (
+    <section>
+      <Navbar />
+
+      <h1></h1>
+    </section>
+  );
+};
