797b027fca207998ea6940dc288d3cdbb96162a5
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-07 08:42:42 +0300

    Remove the word page from home

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Crew from "./pages/Crew";
 import Destination from "./pages/Destination";
-import Homepage from "./pages/Homepage";
+import Homepage from "./pages/Home";
 import Technology from "./pages/Technology";
 
 function App() {
